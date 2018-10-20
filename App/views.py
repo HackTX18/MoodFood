cad9b33681c1991b7d1dda50894aaa7ee4ff776a
BrianK <briankhunchana@gmail.com>
2018-10-20 14:39:10 -0500

    added index

--- App/views.py
+++ App/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import json
+from pprint import pprint
 # Create your views here.
+
+with open("templates/yelp_academic_dataset_review.json") as f:
+    data = json.load(f)
+    print(data)
 
 def index(request):
     return HttpResponse("Hello world")
